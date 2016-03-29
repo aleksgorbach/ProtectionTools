011d53e0e0791a9b26abe91188039e79dbe6278c
Alex Gorbach <aleks.gorbach@gmail.com>
2016-03-29 20:00:14 +0300

    WebUI dependencies configs

---
+++ src/ProtectionTools.WebUI/Controllers/AppController.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtectionTools.WebUI.Controllers {
+    using Microsoft.AspNet.Mvc;
+
+    public class AppController : Controller {
+        // GET: /app/
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
--- src/ProtectionTools.WebUI/Startup.cs
+++ src/ProtectionTools.WebUI/Startup.cs
@@ -12,7 +12,7 @@
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app) {
-            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
             app.UseMvc(config => { config.MapRoute("default", "{controller}/{action}"); });
         }
 
