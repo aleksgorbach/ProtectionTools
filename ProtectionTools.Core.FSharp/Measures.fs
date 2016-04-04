9a6bbe673aba0cc9211382c98b76d00d97ea1c72
Alex Gorbach <aleks.gorbach@gmail.com>
2016-04-04 09:10:48 +0300

    Old stuff removed

--- ProtectionTools.Core.FSharp/Measures.fs
+++
@@ -1,5 +0,0 @@
-﻿namespace ProtectionTools.Core.FSharp
-
-module Measures = 
-    [<Measure>] type ``В``
-    [<Measure>] type ``кВт``
--- ProtectionTools.Core.FSharp/ThreePhaseReceivers.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProtectionTools.Core.FSharp
-
-module ThreePhaseReceivers = 
-    let sqrt3 = sqrt 3.0
-    let CalculatedCurrent (мощность: float<Measures.кВт>) (номинальное_напряжение: float<Measures.В>) cosF coef = 
-        мощность / (sqrt3 * номинальное_напряжение * cosF * coef)
-
-        
-
