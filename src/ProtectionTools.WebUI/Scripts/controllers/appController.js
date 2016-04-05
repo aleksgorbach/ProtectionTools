3788c56016fd7d5a37e9acb8b65255f096ac5a2f
Alex Gorbach <aleks.gorbach@gmail.com>
2016-04-05 21:21:36 +0300

    Remove element functionality

--- src/ProtectionTools.WebUI/Scripts/controllers/appController.js
+++ src/ProtectionTools.WebUI/Scripts/controllers/appController.js
@@ -1,16 +1,15 @@
 ﻿(function() {
-    angular.module("electricityApp").controller("AppController", function($scope, electricity) {
-            $scope.model = {
-                PowerCoef: 1.18,
-                NominalVoltage: 0.38,
-                Elements: []
-            }
-
-
+    angular.module("electricityApp").controller("AppController", function ($scope, electricity) {
+            $scope.model = electricity.getInitialBus();
+        
             $scope.calc = function() {
                 electricity.calc($scope.model).then(function(data) {
                     $scope.model = data;
                 });
+            }
+
+            $scope.removeElement = function(elem) {
+                $scope.model = electricity.removeElement(elem, $scope.model);
             }
 
             $scope.addElement = function() {
--- src/ProtectionTools.WebUI/Scripts/services/electricityService.js
+++ src/ProtectionTools.WebUI/Scripts/services/electricityService.js
@@ -1,9 +1,9 @@
 (function electricityService() {
     var app = angular.module("electricityApp");
 
-    var service = function($http) {
+    var service = function ($http) {
         var calc = function (model) {
-            return $http.post("/app/calc", model).then(function(response) {
+            return $http.post("/app/calc", model).then(function (response) {
                 return response.data;
             });
         }
@@ -12,16 +12,32 @@
             model.Elements.push({
                 ActivePower: 10,
                 Count: 1,
-                UsingCoefficient: 0.8,
-                Cos: 0.6
+                UsingCoefficient: 0.6,
+                Cos: 0.8
             });
             return model;
         }
-        
+
+        var removeElement = function (elem, model) {
+            var index = model.Elements.indexOf(elem);
+            model.Elements.splice(index, 1);
+            return model;
+        }
+
+        var getInitialBus = function () {
+            return {
+                PowerCoef: 1.18,
+                NominalVoltage: 0.38,
+                Elements: []
+            }
+        }
+
         return {
             calc: calc,
-            addElement: addElement
+            addElement: addElement,
+            removeElement: removeElement,
+            getInitialBus: getInitialBus
         }
     }
     app.factory("electricity", service);
-} ());
+}());
